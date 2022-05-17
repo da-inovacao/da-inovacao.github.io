99326a60d1d44f73951065c02899b0ac6ea90a82
Cleianderson <cleianderson13@gmail.com>
2022-05-16 23:41:47 -0300

    feat: add firebase analytics

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,27 @@ import Events from './components/Events';
 import Notices from './components/Notices';
 // import Directorate from './components/Directorate';
 // import Notices from './components/Notices';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ4I0zbZHQw8nQfE9kLucGrEkbGRhokVA",
+  authDomain: "siteda-c1a68.firebaseapp.com",
+  projectId: "siteda-c1a68",
+  storageBucket: "siteda-c1a68.appspot.com",
+  messagingSenderId: "720717782640",
+  appId: "1:720717782640:web:eadd3a9cae470388af1eb5",
+  measurementId: "G-QEQYRB2GBB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
