e179a59df372f0a6537dbeff97fb4c04a1b3b7aa
Cleianderson <cleianderson13@gmail.com>
2022-05-17 00:42:46 -0300

    refact: consuming api

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-// import { Container } from './styles';
+import api from '~/services/Api';
 
 const Events: React.FC = () => {
-  const events = [
-    { href:'#1', title: 'IV Encontro dos Estudantes de Matemática', day: '18', month: 'Mar' },
-    { href:'#2', title: 'III Encontro dos Estudantes de Matemática', day: '2', month: 'Abr' },
-  ]
+  const [events, setEvents] = useState<any>([])
 
-  return (
-    <div className="card-body">
-      {events.map(e => (
+  useEffect(() => {
+    const getEvents = async () => {
+      const { data } = await api.get('/events.json')
+      setEvents(Object.values(data))
+    }
+
+    getEvents()
+  }, [])
+
+  const renderEvents = useCallback(() => {
+    if (events !== null && events.length > 0) {
+      return (events.map((e: any) => (
         <div className='card event mb-4'>
           <div className="row">
-            <div className='col-2 bg-info align-middle rounded-start text-light py-2'>
-              <span className="row justify-content-center font-weight-bold">{e.day}</span>
-              <span className="row justify-content-center card-text">{e.month}</span>
+            <div className='col-2 bg-info rounded-start text-light py-2'>
+              <div className="row text-center">
+                <span className="font-weight-bold">{e.day}</span>
+                <span className="card-text small align-items-center">{e.month} {e.year}</span>
+              </div>
             </div>
-            <div className='col p-2 link-primary'>{e.title}</div>
+            <div className='col p-2'>
+              <span className="link-primary">{e.title}</span>
+            </div>
           </div>
           <a className="card-link-mask" href={e.href}></a>
         </div>
-      ))}
+      )))
+    } else {
+      return (
+        <div className="col d-flex justify-content-center align-items-center">
+          <span className="text-muted center">Sem eventos, por enquanto</span>
+        </div>
+      )
+    }
+  }, [events])
+
+  return (
+    <div className="card-body">
+      {renderEvents()}
     </div>
   )
 }
--- src/components/Notices/index.tsx
+++ src/components/Notices/index.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '~/services/Api'
 
 const Notices: React.FC = () => {
-  const notices = [
-    { href: '#1', title: 'Volta as aulas 2020.1', date: '12/12/9999' },
-    { href: '#2', title: 'Volta as aulas 2022.1', date: '12/12/9999' },
-    { href: '#3', title: 'Escorpião e Cobra Coral são encontrados frequentemente no CEGEN', date: '12/12/9999' },
-  ]
+  const [notices, setNotices] = useState<any>([])
+
+  useEffect(() => {
+    const getNotices = async () => {
+      const { data } = await api.get('/notices.json')
+      setNotices(Object.values(data))
+    }
+
+    getNotices()
+  }, [])
 
   return (
     <div className="card-body">
       {
-        notices.map(notice => (
+        notices.map((notice: any) => (
           <div className="card notice py-2 px-3 border-0 border-bottom mb-2">
             <div className="row">
               <span className='link-primary'>{notice.title}</span>
-              <a className="card-link-mask" href={notice.href}></a>
+              <a className="card-link-mask" target='_blank' rel='noreferrer' href={notice.href}></a>
             </div>
             <div className="row">
               <span className="text-muted small">{notice.date}</span>
