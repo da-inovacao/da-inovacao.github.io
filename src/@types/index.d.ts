932626175d089aebaf19054265d6039b3b27827f
Cleianderson <cleianderson13@gmail.com>
2022-05-20 10:13:04 -0300

    refact: add features in events section

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,6 +1,16 @@
 declare type TEvent = {
-  body: string
+  text: string
   title: string
   date: string
+  banner: string
+  presentations: Presentation[]
+}
+
+declare type Presentation = {
+  date: string
+  title: string
+  presenters: string[]
+  local: string
+  abstract: string
   banner: string
 }
--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -55,20 +55,42 @@ const Events: React.FC = () => {
   }, [events])
 
   return (
-    <div className="card-body">
+    <div className="card-body doc-overview">
       {renderEvents()}
       <Modal show={modalIsVisible} on size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
         <Modal.Header closeButton onHide={() => setModalVisible(false)}>
-          <Modal.Title id="contained-modal-title-vcenter">{modalContent?.title}</Modal.Title>
+          <Modal.Title id="contained-modal-title-vcenter" className="text-primary">{modalContent?.title}</Modal.Title>
         </Modal.Header>
         <div className="col">
           <div className="row">
             <img className='fluid' src={modalContent?.banner} alt="" />
           </div>
         </div>
-        <Modal.Body>{modalContent?.body}</Modal.Body>
+        <Modal.Body>
+          <div className="card-text m-lg-2">{modalContent?.text}</div>
+          <div className="p-lg-4 pt-2">
+            <span className="h5 text-primary">Apresentações</span>
+            <div className='mt-4'>
+              {modalContent?.presentations.map(({ title, banner, date, abstract, local }) => (
+                <div className="card mb-4">
+                  <div className="card-header border-0 d-col">
+                    <span className="card-title-text h6 text-primary">{title}</span>
+                    <span className="text-primary">{new Date(date).toLocaleString('pt-BR')}</span>
+                    <span>Local: {local}</span>
+                  </div>
+                  <div className="card-body container row">
+                    <span className="card-text col text-right">{abstract}</span>
+                    <span className="card-text col-lg-5">
+                      <img src={banner} alt="" className="img-fluid" />
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </Modal.Body>
         <Modal.Footer>
-          <Button onClick={() => setModalVisible(false)}>Close</Button>
+          <Button onClick={() => setModalVisible(false)}>Fechar</Button>
         </Modal.Footer>
       </Modal>
     </div>
