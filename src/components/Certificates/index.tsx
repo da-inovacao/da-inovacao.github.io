e9facdac202c78dda86183b2a5e63c695f842960
Cleianderson <cleianderson13@gmail.com>
2022-06-09 14:05:42 -0300

    fix: certificates links

--- src/components/Certificates/index.tsx
+++ src/components/Certificates/index.tsx
@@ -4,15 +4,15 @@ import React from 'react';
 
 const Certificates: React.FC = () => {
   const certificates = [
-    { title: 'Encontro dos Estudantes de Matemática', href: 'https://drive.google.com/drive/u/4/folders/1m2fRvWnwB-hWKl0XhyYiMzmrfcqnH-Q4' },
-    { title: 'Ciclo de Palestras', href: 'https://drive.google.com/drive/u/4/folders/1m2fRvWnwB-hWKl0XhyYiMzmrfcqnH-Q4' },
+    { title: 'Encontro dos Estudantes de Matemática', href: 'https://drive.google.com/drive/folders/1d5MjXM4b67VqpYsyX9RcQ_XaPxS_2qVh?usp=sharing' },
+    { title: 'Ciclo de Palestras', href: 'https://drive.google.com/drive/folders/12XhuyGR1B9nT422gWEaVYZxZgnU84zry?usp=sharing' },
   ]
 
   return (
     <div className="card-body">
       {certificates.map(certificate => (
-        <div className="link-primary mb-3">{certificate.title}
-          <a className="card-link-mask" target="_blank" rel='noreferrer' href={certificate.href} />
+        <div className="link-primary mb-3">
+          <a className="link-primary" style={{ textDecoration: 'none' }} target="_blank" rel='noreferrer' href={certificate.href}>{certificate.title}</a>
         </div>
       ))}
     </div >
